ff3adbd7d296e6f404aba6fb1cfea011773ee644
chihoyoon <danny.c.yoon@gmail.com>
2020-08-17 18:04:25 -0600

    Create menu page

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const db = require("./lib/db");
 
 const app = express();
 
-// mysql
+//  mysql
 // db.query("SELECT * from menu", function (error, results, fields) {
-//   if (error) {
-//     console.log(error);
-//   }
+//   if (error) throw error;
+
 //   console.log(results);
+
 // });
 
 // db.end();
@@ -29,23 +29,19 @@ app.set("view engine", "hbs");
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-  var name = [];
-  var weblink = [];
-
-  var delivery = [];
-
-  db.query("SELECT * from delivery_service", function (error, results) {
+  db.query("SELECT * FROM delivery_service", function (error, results) {
     if (error) throw error;
 
-    var service = results;
+    var name = [];
+    var weblink = [];
+    var delivery = [];
 
-    for (let i = 0; i < service.length; i++) {
-      name.push(service[i].service_name);
-      weblink.push(service[i].website);
+    for (let i = 0; i < results.length; i++) {
+      name.push(results[i].service_name);
+      weblink.push(results[i].website);
 
       delivery[i] = `<a href="${weblink[i]}" target="_blank">${name[i]}</a>`;
     }
-
     res.render("index", {
       delivery: delivery,
     });
@@ -55,7 +51,59 @@ app.get("/", (req, res) => {
 });
 
 app.get("/menu", (req, res) => {
-  res.render("menu");
+  db.query(
+    "SELECT * FROM menu_class, menu WHERE menu_class.menu_class_id=menu.menu_class_id ",
+    function (error, results) {
+      if (error) throw error;
+
+      var menu_1 = [];
+      var menu_name_1 = [];
+      var price_1 = [];
+      var comment_1 = [];
+      var menu_img_1 = [];
+
+      for (let i = 0; i < results.length; i++) {
+        if (results[i].menu_class_id === 1) {
+          var menu_class_1 = results[i].menu_class_name;
+
+          menu_name_1.push(results[i].menu_name);
+          price_1.push(results[i].price);
+          comment_1.push(results[i].comment);
+          menu_img_1.push(results[i].image_file_name);
+          menu_1[i] = `<table class="table table-dark">
+                        <thead>
+                            <tr>
+                                <th scope="col" colspan="4">
+                                    <h3> ${menu_class_1} </h3>
+                                </th>
+                            </tr>
+                        </thead>
+                
+                        <tbody>
+                           <tr>
+                            <th scope="row" rowlspan="2">
+                              <h4>${menu_name_1[i]}</h4>
+                             </th>
+                            <td>
+                              <h5>\$${price_1[i]}</h5>
+                            </td>
+                            <td rowspan="2"><img src="./img/${menu_img_1[i]}" class="rounded mx-auto d-block" width="150" height="150">
+                            </td>
+                          </tr>
+                           <tr>
+                             <td colspan="2">${comment_1[i]}</td>
+                          </tr>
+                        </tbody>
+                      </table>`;
+        }
+      }
+      res.render("menu", {
+        menu_1: menu_1,
+      });
+    }
+  );
+
+  db.end();
 });
 
 app.get("/blog", (req, res) => {
