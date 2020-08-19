e190ea73bb22d74b512de5cd427ada8f202a1006
chihoyoon <danny.c.yoon@gmail.com>
2020-08-19 10:21:26 -0600

    Fix bug of MySQL code

--- app.js
+++ app.js
@@ -4,6 +4,23 @@ const index = require("./lib/index");
 const menu = require("./lib/menu");
 const blog = require("./lib/blog");
 
+const db = require("./lib/db");
+// db.query("SELECT * FROM delivery_service", function (error, results) {
+//   if (error) throw error;
+
+//   var delivery = [];
+
+//   for (let i = 0; i < results.length; i++) {
+//     delivery[
+//       i
+//     ] = `<a href="${results[i].website}" target="_blank">${results[i].service_name}</a>`;
+//   }
+//   res.render("index", {
+//     delivery: delivery,
+//   });
+// });
+// db.end();
+
 const app = express();
 
 app.engine(
