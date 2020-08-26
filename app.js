44cd3ec157543ba6cc2ccf89ba7676356494237e
chihoyoon <danny.c.yoon@gmail.com>
2020-08-25 18:52:48 -0600

    Organize files

--- app.js
+++ app.js
@@ -1,31 +1,29 @@
 const express = require("express");
 const hbs = require("express-handlebars");
 const bodyParser = require("body-parser");
+
+const app = express();
+
 const index = require("./lib/index");
 const menu = require("./lib/menu");
 const blog = require("./lib/blog");
-const admin_menu = require("./lib/admin_menu");
-const admin_blog = require("./lib/admin_blog");
-const admin_delivery = require("./lib/admin_delivery");
+const a_menu = require("./lib/admin_menu");
+const a_menu_p = require("./lib/admin_menu_post");
+const a_menu_m = require("./lib/admin_menu_modify");
+const a_menu_d = require("./lib/admin_menu_delete");
+const a_menu_c = require("./lib/admin_menu_create");
+const a_blog = require("./lib/admin_blog");
+const a_blog_p = require("./lib/admin_blog_post");
+const a_blog_m = require("./lib/admin_blog_modify");
+const a_blog_c = require("./lib/admin_menu_create");
+const a_blog_d = require("./lib/admin_blog_delete");
+const a_delivery = require("./lib/admin_delivery");
+const a_delivery_m = require("./lib/admin_delivery_modify");
+const a_delivery_d = require("./lib/admin_delvery_delete");
+const a_delivery_c = require("./lib/admin_delivery_create");
 
 const db = require("./lib/db");
-// db.query("SELECT * FROM delivery_service", function (error, results) {
-//   if (error) throw error;
 
-//   var delivery = [];
-
-//   for (let i = 0; i < results.length; i++) {
-//     delivery[
-//       i
-//     ] = `<a href="${results[i].website}" target="_blank">${results[i].service_name}</a>`;
-//   }
-//   res.render("index", {
-//     delivery: delivery,
-//   });
-// });
-// db.end();
-
-const app = express();
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -42,6 +40,9 @@ app.engine(
 app.set("view engine", "hbs");
 
 app.use(express.static(__dirname + "/public"));
+
+var _blog = [];
+var _menu = [];
 
 app.get("/", (req, res) => {
   index._body(req, res);
@@ -86,43 +87,15 @@ app.get("/admin", (req, res) => {
 });
 
 app.get("/admin/menu", (req, res) => {
-  admin_menu._body(req, res);
+  a_menu._body(req, res);
 });
 
-var _menu = [];
-
 app.post("/admin/menu", (req, res) => {
-  db.query(`SELECT * FROM menu WHERE id = ${req.body.id}`, function (
-    err,
-    results
-  ) {
-    if (err) throw err;
-    _menu.push(`<div class="form-row">
-                    <div class="col">
-                        <input type="text" readonly class="form-control" name="id" value="${results[0].id}" />
-                    </div>
-                    <div class="col">
-                        <input type="text" class="form-control" name="class_id" value="${results[0].class_id}" />
-                    </div>
-                    <div class="col">
-                        <input type="text" class="form-control" name="menu" value="${results[0].menu}" />
-                    </div>
-                    <div class="col">
-                        <input type="text" class="form-control" name="price" value="${results[0].price}" />
-                    </div>
-                    <div class="col">
-                        <input type="text" class="form-control" name="comment" value="${results[0].comment}" />
-                    </div>
-                    <div class="col">
-                        <input type="text" class="form-control" name="image" value="${results[0].image}" />
-                    </div>
-                    <div class="col-auto">
-                        <button type="submit" class="btn btn-primary col-md">MODIFY</button>
-                    </div>
-                </div>`);
+  a_menu_p._body(req, res);
+});
 
-    res.redirect("/admin/blog/modify");
-  });
+app.post("/admin/menu/create", (req, res) => {
+  a_menu_c._body(req, res);
 });
 
 app.get("/admin/menu/modify", (req, res) => {
@@ -132,71 +105,27 @@ app.get("/admin/menu/modify", (req, res) => {
 });
 
 app.post("/admin/menu/modify", (req, res) => {
-  db.query(
-    `UPDATE menu SET class_id=${req.body.class_id}, menu='${req.body.menu}', price=${req.body.price}, image='${req.body.image}', comment='${req.body.comment}' Where id=${req.body.id}`,
-    function (err, result) {
-      if (err) throw err;
-      res.redirect("/admin/menu");
-    }
-  );
+  a_menu_m._body(req, res);
 });
 
 app.post("/admin/menu/delete", (req, res) => {
-  db.query(`DELETE FROM menu Where id=${req.body.id}`, function (err, result) {
-    if (err) throw err;
-    res.redirect("/admin/menu");
-  });
-});
-
-app.post("/admin/menu/create", (req, res) => {
-  db.query(
-    `INSERT INTO menu (class_id, menu, price, image, comment) VALUES (${req.body.class_id}, '${req.body.menu}', ${req.body.price}, '${req.body.image}', '${req.body.comment}')`,
-    function (err, result) {
-      if (err) throw err;
-      res.redirect("/admin/menu");
-    }
-  );
+  a_menu_d._body(req, res);
 });
 
 app.get("/admin/blog", (req, res) => {
-  admin_blog._body(req, res);
+  a_blog._body(req, res);
 });
 
-var _blog = [];
-
 app.post("/admin/blog", (req, res) => {
-  db.query(`SELECT * FROM blog WHERE id = ${req.body.id}`, function (
-    err,
-    results
-  ) {
-    if (err) throw err;
-    _blog.push(`<div class="form-row">
-                    <div class="col-sm-3">
-                        <input type="text" readonly class="form-control" name="id" value="${results[0].id}" />
-                    </div>
-                    <div class="col">
-                        <input type="text" class="form-control" name="tittle" value="${results[0].tittle}" />
-                    </div>
-                </div>
-                <div class="form-row">
-                    <div class="col">
-                        <textarea class="form-control" name="description">${results[0].description}</textarea>
-                    </div>
-                </div>
-                <div class="form-row">
-                    <div class="col">
-                      <input type="date" class="form-control" name="date" value="${results[0].date}" />
-                    </div>
-                    <div class="col">
-                        <input type="text" class="form-control" name="author" value="${results[0].author}" />
-                    </div>
-                    <div class="col">
-                        <button type="submit" class="btn btn-primary col-md">MODIFY</button>
-                    </div>
-                </div>`);
+  a_blog_p._body(req, res);
+});
 
-    res.redirect("/admin/blog/modify");
-  });
+app.get("/admin/blog/create", (req, res) => {
+  res.render("admin_blog_create");
+});
+
+app.post("/admin/blog/create", (req, res) => {
+  a_blog_c._body(req, res);
 });
 
 app.get("/admin/blog/modify", (req, res) => {
@@ -206,65 +135,27 @@ app.get("/admin/blog/modify", (req, res) => {
 });
 
 app.post("/admin/blog/modify", (req, res) => {
-  db.query(
-    `UPDATE blog SET tittle='${req.body.tittle}', author='${req.body.author}', date='${req.body.date}', description='${req.body.description}' Where id=${req.body.id}`,
-    function (err, result) {
-      if (err) throw err;
-      res.redirect("/admin/blog");
-    }
-  );
-});
-
-app.get("/admin/blog/create", (req, res) => {
-  res.render("admin_blog_create");
-});
-
-app.post("/admin/blog/create", (req, res) => {
-  db.query(
-    `INSERT INTO blog (tittle, author, date, description) VALUES (${req.body.tittle}, '${req.body.author}', now(), '${req.body.description}')`,
-    function (err, result) {
-      if (err) throw err;
-      res.redirect("/admin/blog");
-    }
-  );
+  a_blog_m._body(req, res);
 });
 
 app.post("/admin/blog/delete", (req, res) => {
-  db.query(`DELETE FROM blog Where id=${req.body.id}`, function (err, result) {
-    if (err) throw err;
-    res.redirect("/admin/blog");
-  });
+  a_blog_d._body(req, res);
 });
 
 app.get("/admin/delivery", (req, res) => {
-  admin_delivery._body(req, res);
-});
-
-app.post("/admin/delivery/modify", (req, res) => {
-  db.query(
-    `UPDATE delivery_service SET service='${req.body.service}', website='${req.body.website}' Where id=${req.body.id}`,
-    function (err, result) {
-      if (err) throw err;
-      res.redirect("/admin/delivery");
-    }
-  );
-});
-
-app.post("/admin/delivery/delete", (req, res) => {
-  db.query(`DELETE FROM menu Where id=${req.body.id}`, function (err, result) {
-    if (err) throw err;
-    res.redirect("/admin/delivery");
-  });
+  a_delivery._body(req, res);
 });
 
 app.post("/admin/delivery/create", (req, res) => {
-  db.query(
-    `INSERT INTO delivery_service (service, website) VALUES ('${req.body.service}', '${req.body.website}')`,
-    function (err, result) {
-      if (err) throw err;
-      res.redirect("/admin/delivery");
-    }
-  );
+  a_delivery_c._body(req, res);
+});
+
+app.post("/admin/delivery/modify", (req, res) => {
+  a_delivery_m._body(req, res);
+});
+
+app.post("/admin/delivery/delete", (req, res) => {
+  a_delivery_d._body(req, res);
 });
 
 app.use((req, res) => {
